dd54bf166b61ce5ce03849a55f79155afa7102fa
bikramgiri <asgbrg.github@gmail.com>
2025-02-13 01:48:08 +0545

    first commit

---
+++ src/Button.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// We can use any name () in the function parameter like props and other name
+const Button = (props) => {
+    const {name} = props
+  return (
+    <button>{name}</button>
+  )
+}
+
+// Used to set the default value of the props
+Button.defaultProps = {
+    name: "Default"
+}
+ // used to check the type of the props
+// Button.propTypes = {
+//     name: PropTypes.string.isRequired
+// }
+
+export default Button
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+function Home(){
+      return (
+            <>
+            <Button name="Register" />
+            <Button name="Login"/>
+            <Button name="Logout"/>
+            <Button  />
+            </>
+      )
+}
+
+export default Home;
