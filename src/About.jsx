dc778e6610257ce87f5be9a1a439f28e3f99f704
bikramgiri <asgbrg.github@gmail.com>
2025-02-14 13:46:20 +0545

    learn about usestate hook

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useState } from "react"
 
 const About = () => {
+  // const state = useState("Bikram")
+  // const firstItem = state[0]
+  // const secondItem = state[1]
+
+  // var count = 0
+
+  // const state = useState(0)
+  // const firstItem = state[0]
+  // const secondItem = state[1]
+
+  const [number, setNumber] = useState({
+    name: "Bikram",
+    address: "Kathmandu"
+  }) // [0, function], syntax for array destructuring
+
+  const decreaseCount = () => {
+    setNumber(number - 1) 
+  }
+
   return (
     <div>
-      <h1>This is the about page</h1>
+      <h1>{number.name}</h1>
+      <button onClick={decreaseCount}>-</button>
     </div>
   )
 }
 
-export default About
+export default About 
